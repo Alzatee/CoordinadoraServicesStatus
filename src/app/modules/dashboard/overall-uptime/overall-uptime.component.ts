fc9f2e21e39903e037828f5d7cf0b1558181d8a9
Andrés Alzate Echeverry <aalzatee@intergrupo.com>
2021-07-18 00:25:31 -0500

    All system operational calcular color con los últimos 30 días, algo de diseño

--- src/app/modules/dashboard/overall-uptime/overall-uptime.component.ts
+++ src/app/modules/dashboard/overall-uptime/overall-uptime.component.ts
@@ -26,10 +26,11 @@ export class OverallUptimeComponent implements OnInit {
       //Para que sea dinámico sin importar cuantos estados o servicios reciba (omite el nombre key que varía) y obtiene la data de cada servicio.
       this.serviceStatus.push(Object.values(element)[0]);
     });
-    this.getServiceStatusDays();
+    this.getAllServiceStatusDays();
   }
 
-  getServiceStatusDays(): void {
+  getAllServiceStatusDays(): void {
+    //obtiene los 30 días de todos los servicios
     this.serviceStatus.forEach(element => {
       this.serviceStatusDays.push(Object.values(element)[2]);
     });
@@ -40,17 +41,17 @@ export class OverallUptimeComponent implements OnInit {
 
   getOfTheDay(): void {
     this.lastDayPercentage = this.utilService.getPercentagesByDays(this.serviceStatusDays, 1);
-    console.log(this.lastDayPercentage);
+    // console.log(this.lastDayPercentage);
   }
 
   getOfTheWeek(): void {
     this.lasteWeekPercentage = this.utilService.getPercentagesByDays(this.serviceStatusDays, 7);
-    console.log(this.lasteWeekPercentage);
+    // console.log(this.lasteWeekPercentage);
   }
 
   getOfTheMonth(): void {
     this.lasteMonthPercentage = this.utilService.getPercentagesByDays(this.serviceStatusDays, 30);
-    console.log(this.lasteMonthPercentage);
+    // console.log(this.lasteMonthPercentage);
   }
 
 
--- src/app/modules/dashboard/systems-operational/systems-operational.component.ts
+++ src/app/modules/dashboard/systems-operational/systems-operational.component.ts
@@ -1,5 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from '@env/environment';
+import { UtilService } from '@shared/util/util.service';
 import { ServiceStatusService } from 'app/app-core/core/services/service-status.service';
 import Swal, { SweetAlertResult } from 'sweetalert2';
 
@@ -12,8 +14,16 @@ export class SystemsOperationalComponent implements OnInit {
   public titleAllsystems = 'All systems operational';
   public lastUpdatedDate: string = '';
   public servicesStatus: any[] = [];
+  public serviceStatus: any[] = [];
+  public serviceStatusDays: any[] = [];
+  public lastMonthPercentage: any;
+  public yellowAlertPercentageGreaterThan = environment.yellow_alert_percentage_greater_than;
+  public aquaAlertPercentageGreaterThan = environment.aqua_alert_percentage_greater_than;
 
-  constructor(private serviceStatusService: ServiceStatusService) { }
+  constructor(
+    private serviceStatusService: ServiceStatusService,
+    private utilService: UtilService
+    ) { }
 
   ngOnInit(): void {
     this.getServicesStatus();
@@ -24,7 +34,7 @@ export class SystemsOperationalComponent implements OnInit {
     .subscribe((result: any) => {
       this.lastUpdatedDate = result.last_updated;
       this.servicesStatus = result.status.apis;
-      
+      this.getServiceStatus();
     }, (error: HttpErrorResponse) => {
       console.log(error);
       Swal.fire({
@@ -33,6 +43,27 @@ export class SystemsOperationalComponent implements OnInit {
         text: error.message
       })
     });
+  }
+
+  getServiceStatus(): void {
+    this.servicesStatus.forEach(element => {
+      //Para que sea dinámico sin importar cuantos estados o servicios reciba (omite el nombre key que varía) y obtiene la data de cada servicio.
+      this.serviceStatus.push(Object.values(element)[0]);
+    });
+    this.getAllServiceStatusDays();
+  }
+
+  getAllServiceStatusDays(): void {
+    //obtiene los 30 días de todos los servicios
+    this.serviceStatus.forEach(element => {
+      this.serviceStatusDays.push(Object.values(element)[2]);
+    });
+    this.getOfTheMonth();
+  }
+
+  getOfTheMonth(): void {
+    this.lastMonthPercentage = this.utilService.getPercentagesByDays(this.serviceStatusDays, 30);
+    // console.log('este', this.lastMonthPercentage);
   }
 
 }
